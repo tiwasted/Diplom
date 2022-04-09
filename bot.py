211582c89265787a069a7c6b2719ec8215bd1e10
Темирлан <meiramov1@icloud.com>
2022-04-09 15:27:25 +0600

    day #2

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
 import random
 import nltk
 import re
+import json
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+
 
 
 def normalize(text):  # Создаем функцию, которая удалит знаки препинания и приведет текст к нижнему регистру
@@ -27,39 +32,79 @@ def getintent(text):  # Понимать намерение по тексту
                 return name
 
 
-question = input()  # Вопрос, который мы задаем боту
+def getanswer(intent):
+    responses = BOT_CONFIG["intents"][intent]["responses"]
+    return random.choice(responses)
 
-database = [
-    {
-        "question": "Помоги мне",
-        "answer": ["Сделай глубокий вдох и выдох", "чем я могу тебе помочь"],
-    },
-    {
-        "question": "У меня депрессия",
-        "answer": "Какой-нибудь практический совет",
-    },
-    {
-        "question": "Как справиться с тревогой",
-        "answer": "Добавить упражнение",
-    }
-]
 
-for pair in database:
-    if ismatching(question, pair["question"]) < 0.4:
-        answer = random.choice(pair["answer"])
-        print(answer)
+def bot(text):
+    intent = getintent(text)
 
-BOT_CONFIG = {
-    "intents": {  # Намерения пользователя
-        "hello": {  # Поздороваться
-            "examples": ["привет", "Добрый день", "Здравствуйте"],
-            "responses": ["Привет", "Вас приветствует Mental Health Бот", "Добро пожаловать"],
-        },
-        "depression": {
-            "examples": ["Что делать когда у меня депрессия", "Депрессия", "У меня депрессия"],
-            "responses": ["Совет", "Вдох-Выдох", "Практический совет"],
-        },
-        "failure_phrases": ["Извените, я Вас не понимаю", "К сожелению я всего лишь бот", "Очень жаль, но я здесь "
-                                                                                          "бессилен"]
-    }
-}
+    if not intent:  # Если намерение не найдено
+        test = vectorizer.transform([text])
+        intent = model.predict(test)[0]
+
+    print("intent =", intent)
+
+    if intent:  # Если намерение найдено - выдать ответ
+        return
+
+    failure_phrases = BOT_CONFIG['failure_phrases']
+    return random.choice(failure_phrases)
+# question = input()  # Вопрос, который мы задаем боту
+#
+# database = [
+#     {
+#         "question": "Помоги мне",
+#         "answer": ["Сделай глубокий вдох и выдох", "чем я могу тебе помочь"],
+#     },
+#     {
+#         "question": "У меня депрессия",
+#         "answer": "Какой-нибудь практический совет",
+#     },
+#     {
+#         "question": "Как справиться с тревогой",
+#         "answer": "Добавить упражнение",
+#     }
+# ]
+#
+# for pair in database:
+#     if ismatching(question, pair["question"]) < 0.4:
+#         answer = random.choice(pair["answer"])
+#         print(answer)
+
+
+# BOT_CONFIG = {
+#     "intents": {  # Намерения пользователя
+#         "hello": {  # Поздороваться
+#             "examples": ["привет", "Добрый день", "Здравствуйте"],
+#             "responses": ["Привет", "Вас приветствует Mental Health Бот", "Добро пожаловать"],
+#         },
+#         "depression": {
+#             "examples": ["Что делать когда у меня депрессия", "Депрессия", "У меня депрессия"],
+#             "responses": ["Совет", "Вдох-Выдох", "Практический совет"],
+#         },
+#         "failure_phrases": ["Извените, я Вас не понимаю", "К сожелению я всего лишь бот", "Очень жаль, но я здесь "
+#                                                                                           "бессилен"]
+#     }
+# }
+
+config_file = open("big_bot_config.json", "r")
+BOT_CONFIG = json.load(config_file)
+X = []
+y = []
+for name, data in BOT_CONFIG["intents"].items():
+    for example in data['examples']:
+        X.append(example)
+        y.append(name)
+
+
+vectorizer = CountVectorizer()  # Можно указать настройки
+vectorizer.fit(X)  # Передаем набор текстов, чтобы векторайзер их проанализировал
+
+CountVectorizer()
+
+X_vectorized = vectorizer.transform(X)  # Трансформирует текст в вектор (наборы чисел)
+
+model = LogisticRegression()  # Настройки
+model.fit()  # Модель научиться по X определять y

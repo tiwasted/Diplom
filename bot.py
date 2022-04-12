3e3ebde173b1d46e1b801bccfefc388543cd5547
Темирлан <meiramov1@icloud.com>
2022-04-12 19:05:14 +0600

    day #4 step #1 Ещё больше изменили бота, теперь он работает лучше

--- bot.py
+++ bot.py
@@ -1,60 +1,112 @@
 import random
 import json
 
-from nltk import edit_distance
+import nltk
 
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
 
 
-with open("BOT_CONFIG.json", "r") as file:
+with open("BOT_CONFIG.json", "r", encoding="utf8") as file:
     BOT_CONFIG = json.load(file)
+
+
+# X = []
+# y = []
+#
+# count = 0
+#
+# for intent in BOT_CONFIG["intents"].keys():
+#     try:
+#         for example in BOT_CONFIG["intents"][intent]["examples"]:
+#             X.append(example)
+#             y.append(intent)
+#     except KeyError:
+#         print(BOT_CONFIG["intents"][intent])
 
 
 X = []
 y = []
-
-count = 0
-
-for intent in BOT_CONFIG["intents"].keys():
-    try:
-        for example in BOT_CONFIG["intents"][intent]["examples"]:
-            X.append(example)
-            y.append(intent)
-    except KeyError:
-        print(BOT_CONFIG["intents"][intent])
+for intent in BOT_CONFIG['intents']:
+    X += BOT_CONFIG['intents'][intent]['examples']
+    y += [intent] * len(BOT_CONFIG['intents'][intent]['examples'])
 
 
-def clean_text(text):
-    output_text = ""
-    for char in text:
+vectorizer = CountVectorizer()
+X_vectorized = vectorizer.fit_transform(X)
+
+
+model = LogisticRegression(random_state=42)
+model.fit(X_vectorized, y)
+
+
+# print(model.predict(vectorizer.transform(["Депрессия"])))
+
+
+def clean(text):
+    clean_text = ''
+    for char in text.lower():
         if char in "абвгдеёжзийклмнопрстуфхцчшщъыьэюя":
-            output_text = output_text + char
-    return output_text
+            clean_text = clean_text + char
+    return clean_text
 
 
-def get_intent(input_text):
-    for intent in BOT_CONFIG["intents"].keys():
-        for example in BOT_CONFIG["intents"][intent]["examples"]:
-            text1 = input_text.lower()
-            text2 = example.lower()
-            if edit_distance(text1, text2) / max(len(text1), len(text2)) < 0.34:
+def compare(s1, s2):
+    return nltk.edit_distance(s1, s2) / ((len(s1) + len(s2)) / 2) < 0.4
+
+def get_intent(question):
+    for intent in BOT_CONFIG['intents']:
+        for example in BOT_CONFIG['intents'][intent]['examples']:
+            if compare(clean(example), clean(question)):
                 return intent
-    return "Not Found"
+    return 'Не удалось определить интент'
 
 
-def bot(input_text):
-    intent = get_intent(input_text)
-    if intent == "Not found":
-        return "Мы вас не поняли"
+# def get_intent(input_text):
+#     for intent in BOT_CONFIG["intents"].keys():
+#         for example in BOT_CONFIG["intents"][intent]["examples"]:
+#             text1 = input_text.lower()
+#             text2 = example.lower()
+#             if edit_distance(text1, text2) / max(len(text1), len(text2)) < 0.34:
+#                 return intent
+#     return "Not Found"
+
+
+# def bot(input_text):
+#     intent = get_intent(input_text)
+#     if intent == "Not found":
+#         return "Мы вас не поняли"
+#     else:
+#         return random.choice(BOT_CONFIG["intents"][intent]["responses"])
+
+
+def get_intent_by_ml(text):
+    return model.predict(vectorizer.transform([text]))[0]
+
+
+def bot(ml):
+    question = input()
+
+    if ml:
+        intent = get_intent_by_ml(question)
     else:
-        return random.choice(BOT_CONFIG["intents"][intent]["responses"])
+        intent = get_intent(question)
 
+    if intent != 'Не удалось определить интент':
+        print(random.choice(BOT_CONFIG['intents'][intent]['responses']))
+    else:
+        print(random.choice(BOT_CONFIG['falture_phrases']))
 
-input_text = ""
-print("Для выхода из диалога напишите Пока")
 
 while True:
-    input_text = input()
-    if input_text == "Пока":
-        break
-    print(bot(input_text))
+    bot(True)
+
+
+# input_text = ""
+# print("Для выхода из диалога напишите Stop")
+#
+# while True:
+#     input_text = input()
+#     if input_text == "Stop":
+#         break
+#     print(bot(input_text))
